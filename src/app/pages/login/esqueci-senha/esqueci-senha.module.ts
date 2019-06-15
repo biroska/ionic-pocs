093d97111942d8c1b9308bbbe6730f8b3f6b3039
Aimberê Galdino <biroska@gmail.com>
2019-06-14 21:42:58 -0300

    Criacao da pagina de esqueci minha senha e router para login

--- src/app/pages/login/esqueci-senha/esqueci-senha.module.ts
+++ src/app/pages/login/esqueci-senha/esqueci-senha.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { EsqueciSenhaPage } from './esqueci-senha.page';
+import {EsqueciSenhaPage} from './esqueci-senha.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: EsqueciSenhaPage
-  }
+    {
+        path: '',
+        component: EsqueciSenhaPage
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [EsqueciSenhaPage]
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        IonicModule,
+        RouterModule.forChild(routes)
+    ],
+    declarations: [EsqueciSenhaPage]
 })
-export class EsqueciSenhaPageModule {}
+export class EsqueciSenhaPageModule {
+}
--- src/app/pages/login/esqueci-senha/esqueci-senha.page.ts
+++ src/app/pages/login/esqueci-senha/esqueci-senha.page.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-esqueci-senha',
-  templateUrl: './esqueci-senha.page.html',
-  styleUrls: ['./esqueci-senha.page.scss'],
+    selector: 'app-esqueci-senha',
+    templateUrl: './esqueci-senha.page.html',
+    styleUrls: ['./esqueci-senha.page.scss'],
 })
 export class EsqueciSenhaPage implements OnInit {
 
-  constructor() { }
+    private router: Router;
+    public formulario: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(private fBuilder: FormBuilder,
+                private rota: Router) {
+
+        this.router = rota;
+
+        this.formulario = this.fBuilder.group(
+            {
+                username: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+                password: ['', Validators.compose([Validators.required])],
+                confirmPassword: ['', Validators.compose([Validators.required])],
+            }
+        );
+
+    }
+
+    ngOnInit() {
+    }
+
+    submitForm() {
+        console.log(this.formulario.value);
+        this.router.navigate(['/login']);
+    }
+
+    log() {
+        console.log(this.formulario.value);
+        console.log(this.formulario.valid);
+        console.log(this.formulario.controls.username.errors);
+        console.log(this.formulario.controls.password.errors);
+        console.log(this.formulario.controls.confirmPassword.errors);
+    }
 
 }
